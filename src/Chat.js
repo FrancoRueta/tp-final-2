b274d29e0e87f932aeee58713717f289fc66dfc8
frueta <askjfuhje@hotmail.com>
2021-11-18 21:03:05 -0300

    trabajo finalizado

--- src/Chat.js
+++ src/Chat.js
@@ -6,44 +6,74 @@ import Respuesta from './Respuesta';
 
 const Chat = () => {
 
+    const usuarioRandom = Math.floor(Math.random() * 1000) + 1;
+
+    const [msgActual, setMsgActual] = React.useState('');
+    const [respuesta, setRespuesta] = React.useState({});
+
+
+    const [listaMensajes, setListaMensajes] = React.useState([
+        {
+            texto: '',
+            color: ''
+        }]);
+
+    const [listaRespuestas, setListaRespuestas] = React.useState([
+        {
+            texto: '',
+            color: ''
+        }]);
+
     
-    //definimos el state que se va a encargar de almacenar todos los mensajes
-    const [mensajesDelChat, setMensajesDelChat] = React.useState([
-    ]);
 
-    //definimos el state que se va a encargar de almacenar el mensaje que se esta escribiendo
-    const [mensaje, setMensaje] = React.useState('');
-
-
-    //funcion que se activa cada vez que el input se actualiza
-    const actualizarMensajeActual = (event) => {
-        event.preventDefault();
-        setMensaje(event.target.value);
+    const actualizarMensaje = (e) => {
+        e.preventDefault();
+        setMsgActual(e.target.value);
     }
 
+    const generarRespuesta = async() => {
+        try{
+            const botUrl =  `https://fathomless-mesa-19690.herokuapp.com/http://api.brainshop.ai/get?bid=161003&key=XWultLaoaGjbh1ZQ&uid=${usuarioRandom}&msg=${msgActual}`;
+            const response = await fetch(botUrl);
+            const data = await response.json();
+            setRespuesta(data.cnt);
+            setListaMensajes([...listaMensajes, {texto: "TU: "+msgActual, color: 'verde'}]);
+            setListaRespuestas([...listaRespuestas, {texto: "BOT: "+ data.cnt, color: "celeste"}]);
+        }catch(error){
+            console.log(error);
+        }
 
-    //funcion que se encarga de agregar un mensaje al chat
-    const agregarMensajeAlChat = (event) => {
-        event.preventDefault();
-        setMensajesDelChat([...mensajesDelChat, mensaje]);
-        setMensaje('');
     }
+
+    const tamanioMensaje = listaMensajes.length;
+
+    const generarChat = () => {
+        const chat = [];
+        for(let i = 1; i < tamanioMensaje; i++){
+            chat.push(listaMensajes[i]);
+            chat.push(listaRespuestas[i]);
+            if(chat.length > 9){
+                let elementoBorrado = chat.shift();
+                let elementoBorrado2 = chat.shift();
+            }
+        }
+        return chat;
+    }
+
+    const chatRender = (listaDeMSG) => (
+        <Fragment>
+          {listaDeMSG.map(msgRender => (
+            <Mensaje texto={msgRender?.texto} color={msgRender?.color}/>
+          ))}
+        </Fragment>
+      ); 
 
     return ( 
         <Col className="chat-tarjeta-principal">
             <Row className="chat-row">
                 <ul className="no-padding-left">
                     {
-                        mensajesDelChat.map((mensaje) => (
-                            <Fragment>
-                                <Mensaje
-                                    mensaje={mensaje}
-                                />
-                                <Respuesta
-                                    mensaje={mensaje}
-                                />
-                            </Fragment>
-                        ))
+                        chatRender(generarChat())
                     }
                 </ul>
             
@@ -56,11 +86,10 @@ const Chat = () => {
                     name='mensajeActual'
                     className='chat-input-text'
                     placeholder='Pruebe diciendo: hello! how are you?'
-                    onChange={actualizarMensajeActual}
-                    value={mensaje}
+                    onChange={actualizarMensaje}
                 />
 
-                <button type="submit" onClick={agregarMensajeAlChat} className="chat-input-button"> Enviar </button>
+                <button type="submit" onClick={generarRespuesta} className="chat-input-button"> Enviar </button>
             </Row>
         </Col>
         );
--- src/Mensaje.js
+++ src/Mensaje.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './styles/mensaje.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Mensaje = ({mensaje}) => {
+const Mensaje = ({texto,color}) => {
     return ( 
-        <li className="celeste">TU: {mensaje}</li>
+        <li className={color}>{texto}</li>
      );
 }
  
